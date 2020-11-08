82cc6c9ad4cb226319a46194df61f55617fbb067
RafaelLima99 <rafaellima.ti.99@gmail.com>
2020-11-08 17:27:39 -0200

    upload

---
+++ js/open_form.js
@@ -0,0 +1,13 @@
+function open_login(){
+    var form_entrar = document.getElementById('form-login');
+    var form_cadastro = document.getElementById('form-cadastro');
+    form_entrar.style.display = 'inline';
+    form_cadastro.style.display = 'none';
+}
+function open_cadastro(){
+    var form_entrar = document.getElementById('form-login');
+    var form_cadastro = document.getElementById('form-cadastro');
+    form_entrar.style.display = 'none';
+    form_cadastro.style.display = 'inline';
+}
+// QUE MERDAAA KKKKKKKKKKKKKKKKKKKKKKKKKKKKKK
